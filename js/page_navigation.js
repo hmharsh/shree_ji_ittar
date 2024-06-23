556347a7e8bb69435be7d217321c78c91ab1cb65
harshit <hmharsh3@gmail.com>
2024-06-23 16:09:56 +0530

    fix the redirection for merchant center products

--- js/page_navigation.js
+++ js/page_navigation.js
@@ -1,5 +1,6 @@
 productUrlMap.forEach(function(item){
     if (window.location.hash.endsWith(item.name)){
-        window.location.url = "https://shreejiittar.mini.store/products/"+item.id;
+        console.log(item.name)
+        window.location.href = "https://shreejiittar.mini.store/products/"+item.id;
     }
 })
