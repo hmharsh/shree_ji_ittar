b777db0daa02973f7749ea0cc3ec7d4c7580764c
harshit <hmharsh3@gmail.com>
2024-06-23 15:12:12 +0530

    add new page info

--- js/product_page_js.js
+++ js/product_page_js.js
@@ -32,45 +32,50 @@ function loadFile(filePath) {
     return result;
   }
 
-// Load and assign product details
+  // Load and assign product details
+  product_name="N/A"
+  if (window.location.href.split('?')[1].split('=')[0] ==  "product_name"){
+    product_name=window.location.href.split('?')[1].split('=')[1]
+  }
   document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("product_title").textContent=location.href.split('#')[1].replaceAll("_", " ").toUpperCase()
-  document.getElementById("product_img").src = "./images/product/"+location.href.split('#')[1]+".jpeg";
+  document.getElementById("product_title").textContent=product_name.replaceAll("_", " ").toUpperCase()
+  document.getElementById("product_img").src = "./images/product/"+product_name+".jpeg";
 
 
   // Identify product_type
   product_type=undefined
-  if (window.location.hash.includes("_cooler_perfume")) {
+  if (product_name.includes("_cooler_perfume")) {
       product_type = "cooler_perfume";
-      if (["#khus_cooler_perfume"].includes(window.location.hash)){
+      if (["#khus_cooler_perfume"].includes(product_name)){
         ([{"5ml": "140"}]).forEach(populateSizetable)
       } else {
         ([{"5ml": "80"}]).forEach(populateSizetable)
       }
-    } else if (window.location.hash.includes("_combo")) {
+    } else if (product_name.includes("_combo")) {
       product_type = "combo";
       ([{"6ml each": "380"}]).forEach(populateSizetable)
-    } else if (window.location.hash.includes("_perfume")) {
+    } else if (product_name.includes("_perfume")) {
       product_type = "perfume";
       ([{"8ml pocket friendly": "80"}, {"10ml like pencil": "80"}]).forEach(populateSizetable)
-    } else if (window.location.hash.includes("_ittar")) {
+    } else if (product_name.includes("_ittar")) {
       product_type = "ittar";
-      if (["#kesar_chandan_ittar", "#champa_chameli_ittar", "#kesar_gulab_ittar", "#kesar_gulab_chandan_ittar", "#kesar_mogra_chandan_ittar", "#kesar_mogra_ittar", "#khus_cooler_perfume", "#chandan_cooler_perfume", "#kaweda_chandan_ittar"].includes(window.location.hash)){
+      if (["#kesar_chandan_ittar", "#champa_chameli_ittar", "#kesar_gulab_ittar", "#kesar_gulab_chandan_ittar", "#kesar_mogra_chandan_ittar", "#kesar_mogra_ittar", "#khus_cooler_perfume", "#chandan_cooler_perfume", "#kaweda_chandan_ittar"].includes(product_name)){
         ([{"2ml": "50"},{"3ml": "70"},{"5ml-dropper": "80"}, {"6ml": "100"}, {"12ml": "150"} ]).forEach(populateSizetable)
-      } else if (["#khus_ittar", "#kesar_ittar"].includes(window.location.hash)) {
+      } else if (["#khus_ittar", "#kesar_ittar"].includes(product_name)) {
         ([{"2ml": "50"},{"3ml": "70"},{"5ml-dropper": "120"}, {"6ml": "120"}, {"12ml": "200"} ]).forEach(populateSizetable)
       } else {
         ([{"2ml": "40"},{"3ml": "50"},{"5ml-dropper": "80"}, {"6ml": "80"}, {"12ml": "120"} ]).forEach(populateSizetable)
       }
-    } else if (window.location.hash.includes("_rollon")) {
+    } else if (product_name.includes("_rollon")) {
       product_type = "rollon";
       ([{"2ml": "40"},{"3ml": "50"}, {"6ml": "80"}, {"12ml": "120"} ]).forEach(populateSizetable)
     }
      else {
+        alert("Product not found: possibly the link you clicked on is not working, redirecting to our online mini store, please try to search product there")
         window.location = "https://shreejiittar.mini.store"
     }
     addExtraImages(product_type)
     
     // Load description
-    document.getElementById("product_description").textContent=loadFile("./product_description/"+location.href.split('#')[1]+".txt");
+    document.getElementById("product_description").textContent=loadFile("./product_description/"+product_name+".txt");
 });
