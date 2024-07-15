cf28d3fadda76a5f85ff20af913dadb0be1bd649
harshit <hmharsh3@gmail.com>
2024-07-15 09:46:15 +0530

    reverting back

--- js/about_us.js
+++ js/about_us.js
@@ -7,27 +7,21 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("speak_in_english").onclick = function(){
     if(!audio_hindi.paused) {
         audio_hindi.pause()
-        document.getElementById("speak_in_hindi").innerHTML="<u>Hindi</u>"
     }
     if(!audio_english.paused) {
        audio_english.pause()
-       document.getElementById("speak_in_english").innerHTML="<u>English</u>"
     } else {
         audio_english.play();
-        document.getElementById("speak_in_english").innerHTML="<u>⏯️ Pause</u>"
     } 
   };
   document.getElementById("speak_in_hindi").onclick = function(){
     if(!audio_english.paused) {
         audio_english.pause()
-        document.getElementById("speak_in_english").innerHTML="<u>English</u>"
      }
     if(!audio_hindi.paused) {
         audio_hindi.pause()
-        document.getElementById("speak_in_hindi").innerHTML="<u>Hindi</u>"
      } else {
          audio_hindi.play();
-         document.getElementById("speak_in_hindi").innerHTML="<u>⏯️ Pause</u>"
      } 
   }
 })
--- js/load_header_footer.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-$("#header").load("header.html"); 
-$("#footer").load("footer.html");
-})
