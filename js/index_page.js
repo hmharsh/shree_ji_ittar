0bdf91604cc1dffc7a74748a4bbb9a9deaddc977
harshit <hmharsh3@gmail.com>
2025-01-03 13:06:31 +0530

    update header

--- js/index_page.js
+++ js/index_page.js
@@ -1,7 +1,7 @@
 
-const headings = ["Shree Ji Ittar", "श्रीजी इत्र"];
-var random = "1"
-setInterval(function () {
-random = Math.floor(Math.random() * headings.length);
-document.getElementById("headText").innerText = headings[random]
-}, 1500);
+// const headings = ["Shree Ji Ittar", "श्रीजी इत्र"];
+// var random = "1"
+// setInterval(function () {
+// random = Math.floor(Math.random() * headings.length);
+// document.getElementById("headText").innerText = headings[random]
+// }, 1500);
