25535413fc122a50af0572ff008112cf65c7be33
harshit <hmharsh3@gmail.com>
2025-03-15 11:13:00 +0530

    test completed

--- js/page_navigation.js
+++ js/page_navigation.js
@@ -1,6 +1,5 @@
 productUrlMap.forEach(function(item){
     if (window.location.hash.endsWith(item.name)){
-        console.log(item)
-        // window.location.href = "https://shreejiittar.mini.store/products/"+item.id;
+        window.location.href = "https://shreejiittar.mini.store/products/"+item.id;
     }
 })
