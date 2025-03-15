ad75b64f2aea0603c03a0da7015053b0ee1beb41
harshit <hmharsh3@gmail.com>
2025-03-15 11:20:21 +0530

    add new tab feature

--- js/page_navigation.js
+++ js/page_navigation.js
@@ -1,5 +1,6 @@
 productUrlMap.forEach(function(item){
     if (window.location.hash.endsWith(item.name)){
-        window.location.href = "https://shreejiittar.mini.store/products/"+item.id;
+        // window.location.href = "https://shreejiittar.mini.store/products/"+item.id;
+        window.open("https://shreejiittar.mini.store/products/"+item.id, "_blank");
     }
 })
