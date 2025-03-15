ab9de403d4c66607afbabd30dea38f5a5a83e3f0
harshit <hmharsh3@gmail.com>
2025-03-15 11:34:40 +0530

    fix navigation

--- js/page_navigation.js
+++ js/page_navigation.js
@@ -1,6 +1,9 @@
 productUrlMap.forEach(function(item){
     if (window.location.hash.endsWith(item.name)){
-        // window.location.href = "https://shreejiittar.mini.store/products/"+item.id;
-        window.open("https://shreejiittar.mini.store/products/"+item.id, "_blank");
+        const timestamp = new Date().getTime();
+        const currentUrl = "https://shreejiittar.mini.store/products/"+item.id;
+        const newUrl = currentUrl.includes('?') ? `${currentUrl}&t=${timestamp}` : `${currentUrl}?t=${timestamp}`;
+        window.location.href = newUrl;
+        //window.open("https://shreejiittar.mini.store/products/"+item.id, "_blank");
     }
 })
